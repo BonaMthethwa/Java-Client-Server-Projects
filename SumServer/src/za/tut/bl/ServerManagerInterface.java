1bb2675f32532b58eddb1ca853ec7656b9737974
Bonani Mthethwa <zbmthethwa01@gmail.com>
2026-07-17 23:35:18 +0200

    Delete SumServer directory

--- SumServer/src/za/tut/bl/ServerManagerInterface.java
+++
@@ -1,9 +0,0 @@
-
-package za.tut.bl;
-
-
-public interface ServerManagerInterface {
-    public Integer add(Integer num1,Integer num2);
-    public Integer substract(Integer num1, Integer num2);
-    public Integer Multiply(Integer num1, Integer num2);
-}
